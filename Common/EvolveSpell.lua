37dad73d13e2d2c8e975bb44d371c9bedd7a5a5e
PvPSuite <pvpsuite@gmail.com>
2015-07-23 02:30:37 +0300

    Update EvolveSpell.lua
    5.14

--- Common/EvolveSpell.lua
+++ Common/EvolveSpell.lua
@@ -4,21 +4,30 @@ function EvolveSpell(spID)
 	end;
 	
 	local pOffsets = {
-		[_Q] = {0x7D, 0x07},
-		[_W] = {0x12, 0x06},
-		[_E] = {0x76, 0x05},
-		[_R] = {0x9C, 0x04},
+		[_Q] = 0xAA,
+		[_W] = 0xAB,
+		[_E] = 0xAC,
+		[_R] = 0xAD,
 	};
 	
-	local eP = CLoLPacket(0x00A2);
-	eP.vTable = 0xF72190;
+	local eP = CLoLPacket(0x009C);
+	eP.vTable = 0xEE6D00;
 	eP:EncodeF(myHero.networkID);
-	eP:Encode1(pOffsets[spID][1]);
-	eP:Encode4(0xA4A4A4A4);
-	eP:Encode4(0x4C4C4C4C);
-	eP:Encode1(0x82);
-	eP:Encode4(0x48484848);
-	eP:Encode1(pOffsets[spID][2]);
-	eP:Encode4(0x00000000);
+	for I = 1, 4 do
+		eP:Encode1(0xB4);
+	end;
+	for I = 1, 4 do
+		eP:Encode1(0x69);
+	end;
+	for I = 1, 4 do
+		eP:Encode1(0x09);
+	end;
+	eP:Encode1(pOffsets[spID]);
+	eP:Encode1(0x43);
+	eP:Encode1(0x88);
+	for I = 1, 4 do
+		eP:Encode1(0x00);
+	end;
+	
 	SendPacket(eP);
 end;
